ce143eb08ca217db82fb3256e0ec55b29a65a5a4
RStenzhorn <business.rst@outlook.de>
2025-04-15 20:05:38 +0200

    Enhance integration test for customer creation API with improved response validation

--- src/test/java/de/rjst/cs/api/ApiIT.java
+++ src/test/java/de/rjst/cs/api/ApiIT.java
@@ -4,13 +4,16 @@ import de.rjst.cs.CustomerController;
 import de.rjst.cs.TestcontainersConfiguration;
 import io.restassured.filter.log.LogDetail;
 import org.junit.jupiter.api.Test;
-import org.openapitools.client.ApiClient;
 import org.openapitools.client.api.CustomerControllerApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
@@ -26,15 +29,22 @@ public class ApiIT {
     void testApiClient() {
         final var createCustomerDto = new CreateCustomerDto();
         createCustomerDto.setBirthDate(LocalDate.now());
-        createCustomerDto.setFirstName("string");
-        createCustomerDto.setLastName("string");
+        createCustomerDto.setFirstName("anyFirstName");
+        createCustomerDto.setLastName("anyLastName");
         createCustomerDto.setEmail(null);
         customerController.createCustomer(createCustomerDto);
 
         customerControllerApi.getCustomerById()
                 .idPath(1L)
-                .respSpec(response -> response.log(LogDetail.ALL))
-                .executeAs(response -> response);
+                .respSpec((spec) -> spec
+                        .log(LogDetail.ALL)
+                        .expectStatusCode(HttpStatus.OK.value())
+                        .expectBody("id", equalTo(1))
+                        .expectBody("firstName", equalTo("anyFirstName"))
+                        .expectBody("lastName", equalTo("anyLastName"))
+                        .expectBody("birthDate", equalTo(LocalDate.now().toString()))
+                        .expectBody("email", nullValue())
+                );
     }
 
 }
