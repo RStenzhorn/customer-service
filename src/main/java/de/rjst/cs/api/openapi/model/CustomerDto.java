17da60beb5362663ba2330e52a14748aabb067c6
RStenzhorn <business.rst@outlook.de>
2025-05-09 00:22:56 +0200

    init

--- src/main/java/de/rjst/cs/api/openapi/model/CustomerDto.java
+++ src/main/java/de/rjst/cs/api/openapi/model/CustomerDto.java
@@ -1,5 +1,7 @@
 package de.rjst.cs.api.openapi.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -8,17 +10,22 @@ import lombok.Data;
 @Data
 public class CustomerDto {
 
+    @Schema(description = "The ID of the customer", requiredMode = RequiredMode.REQUIRED)
     @NotNull
     private Long id;
 
+    @Schema(description = "The first name of the customer", requiredMode = RequiredMode.REQUIRED)
     @NotBlank
     private String firstName;
 
+    @Schema(description = "The last name of the customer", requiredMode = RequiredMode.REQUIRED)
     @NotBlank
     private String lastName;
 
+    @Schema(description = "The birth date of the customer", requiredMode = RequiredMode.REQUIRED)
     @NotNull
     private LocalDate birthDate;
 
+    @Schema(description = "The email of the customer")
     private String email;
 }
